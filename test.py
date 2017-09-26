38dab29016803392221a508816eb12d8926b1a65
TakashiNomura <nomufamily@gmail.com>
2017-09-26 16:50:15 +0900

    fix
    fix

--- test.py
+++ test.py
@@ -1,7 +1,30 @@
+
 try:
     while True:
-        indata = int(input().strip().upper())
-        print(indata)
+        a = input().strip().upper()
+        #rows = 0
+        if a.isdigit() == True:
+            X = int(a) # 縦横X
+            if X % 2 == 0 :
+                # 偶数
+                print("invalid")
+            else:
+                # 奇数 
+                for i in range(X):
+                    output = ""
+                    for j in range(X):
+                        c = X//2    # 中間
+                        if j == i and j <= c:
+                            output += "y"
+                        elif i > c and j==c:
+                            output += "y"
+                        elif i < c and j == X - (i + 1):
+                            output += "y"
+                        else:
+                            output += "."
+                    print(output)
+        else:
+            print("invalid")
 except EOFError:
     pass
 
