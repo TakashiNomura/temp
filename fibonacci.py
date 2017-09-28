0ecc265925c37d052180cad44cfd711ed8f38c5f
TakashiNomura <nomufamily@gmail.com>
2017-09-28 17:09:53 +0900

    Add files via upload
    hoge

--- fibonacci.py
+++ fibonacci.py
@@ -8,20 +8,30 @@ def repeat_func(num, func, result_list):
 def fibonacci(n, a=1, b=0):
     return b if n < 1 else fibonacci(n-1, a+b, a)
 
-def fibo(n):
-    a1, a2 = 1, 0
-    while n > 0:
-        a1, a2 = a1 + a2, a1
-        n -= 1
-    return a1
+def fib2(n):
+    if n<=1:
+        return n
+    result = [1,0,0,1]
+    matrix = [1,1,1,0]
+    while n>0:
+        if n%2:
+            result=mul(matrix, result)
+        matrix=mul(matrix, matrix)
+        n //= 2
+    return result[2]
+def mul(a, b):
+    return [a[0]*b[0] + a[1]*b[2],
+            a[0]*b[1] + a[1]*b[3],
+            a[2]*b[0] + a[3]*b[2],
+            a[2]*b[1] + a[3]*b[3]]
+
 '''
 sequens_list = []
-repeat_func(101, fibonacci, sequens_list)
-
+repeat_func(979612319, fibonacci, sequens_list)
 print(sequens_list)
 
 sequens_list.reverse()
 print(sequens_list)
 '''
-f = fibo(655365569)
-print(f)
+#print(fib2(979612319))
+print(fib2(97))
