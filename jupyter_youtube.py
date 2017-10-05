7a1f0509c2922c1b7d43cbb7e8d714336babab06
TakashiNomura <nomufamily@gmail.com>
2017-10-05 10:01:01 +0900

    Update jupyter_youtube.py

--- jupyter_youtube.py
+++ jupyter_youtube.py
@@ -4,6 +4,6 @@ from IPython.core.display import HTML
 html=""
 for url in search("FF15", num=10, stop=10, tpe="vid"):
     if "?v=" in url:
-        id=ur.split("?v=")[1]
+        id=url.split("?v=")[1]
         html+='<iframe src="//www.youtube.com/embed/{}" width="560" height="315" frameborder="0" allowfullscreen></iframe>'.format(id)
 HTML(html)
